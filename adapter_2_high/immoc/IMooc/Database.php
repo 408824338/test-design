ca3d3454795252a9c31af207effc05d747724cb2
cmk02 <408824338@qq.com>
2017-01-22 07:22:43 +0800

    完善接口,不用在客户端那里定义参数

--- adapter_2_high/immoc/IMooc/Database.php
+++ adapter_2_high/immoc/IMooc/Database.php
@@ -9,10 +9,9 @@
 namespace IMooc;
 
 
-
 class Database
 {
-    private $private = 'PDO';   // 调用的方式有 MySQL MySQLi  PDO
+    private static $private = 'PDO';   // 调用的方式有 MySQL MySQLi  PDO
 
     protected static $db;
 
@@ -25,25 +24,24 @@ class Database
         if (self::$db) {
             return self::$db;
         } else {
+            switch (self::$private) {
+                case 'MySQL';
+                    self::$db = new IMooc\Database\MySQL();
+                    return self::$db;
+                    break;
 
-            self::$db =  new IMooc\Database\PDO();
-            return self::$db;
+                case 'MySQLi';
+                    self::$db = new IMooc\Database\MySQLi();
+                    return self::$db;
+                    break;
+                case 'PDO';
+                    self::$db = new Database\PDO();
+                    self::$db ->connect('127.0.0.1','root','123456','test');
+                   return self::$db;
+                    break;
+            }
         }
 
     }
 
-    function where($where)
-    {
-        return $this;
-    }
-
-    function order($order)
-    {
-        return $this;
-    }
-
-    function limit($limit)
-    {
-        return $this;
-    }
 }
--- adapter_2_high/immoc/index.php
+++ adapter_2_high/immoc/index.php
@@ -13,11 +13,11 @@ spl_autoload_register('\\IMooc\\Loader::autoload');
  * run http://127.0.0.1:82/design/adapter_1_simple/immoc/index.php
  * 1.高级一些,如在前面不用定义指定什么,后端可以通过配置来定义
  */
-$db = new IMooc\Database\PDO();
-$db->connect('127.0.0.1','root','123456','test');
-//$arr = $db->query('show databases');
+
+$db =  IMooc\Database::getInstance();
+
 $rs = $db->query('select * from dj_check_sheet');
 while($row = $rs -> fetch()){
-   // print_r($row);
+   print_r($row);
 }
 $db->close();
